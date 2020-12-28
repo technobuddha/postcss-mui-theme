27fe5cee0fd49e1f7023c5004a1abc2165dd28bb
Phil Hill <phil@technobuddha.com>
2020-12-28 16:28:06 -0500

    Handle multiple calls on single line

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const reduceFunctionCall    = require('reduce-function-call');
-const get                   = require('lodash/get')
+const get                   = require('lodash/get');
 
 module.exports = (opts = {}) => {
     const theme = opts.theme || {};
@@ -10,18 +10,29 @@ module.exports = (opts = {}) => {
             root.walkDecls(
                 decl => {
                     if(decl.value) {
-                        if(decl.value.indexOf('mui-theme(') !== -1) {
-                            decl.value = reduceFunctionCall(
-                                decl.value,
-                                'mui-theme',
-                                body => get(theme, body.replace(/-/g, '.'))
-                            );
-                        } else if (decl.value.indexOf('mui-spacing(') !== -1) {
-                            decl.value = reduceFunctionCall(
-                                decl.value,
-                                'mui-spacing',
-                                body => `${theme.spacing(Number.parseFloat(body))}px`
-                            )
+                        for(;;)
+                        {
+                            const idxTheme = decl.value.indexOf('mui-theme(');
+                            const idxSpace = decl.value.indexOf('mui-spacing(');
+
+                            if(idxTheme === -1 && idxSpace === -1)
+                                break;
+
+                            if(idxTheme !== -1) {
+                                decl.value = reduceFunctionCall(
+                                    decl.value,
+                                    'mui-theme',
+                                    body => get(theme, body.replace(/-/g, '.'))
+                                );
+                            }
+
+                            if(idxSpace !== -1) {
+                                decl.value = reduceFunctionCall(
+                                    decl.value,
+                                    'mui-spacing',
+                                    body => `${theme.spacing(Number.parseFloat(body))}px`
+                                )
+                            }
                         }
                     }
                 }
